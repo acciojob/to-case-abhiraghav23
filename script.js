5534394209496a61a6a66e52d426ec906b68d460
Abhishek Raghav <115450330+abhiraghav23@users.noreply.github.com>
2024-11-28 13:02:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ return text.toLowerCase() + '-' + text.toUpperCase();
 
 // DO not change the code below
 
-// const text = prompt("Enter text:");
-// alert(toCase(text));
+const text = prompt("Enter text:");
+alert(toCase(text));
